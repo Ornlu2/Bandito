7d13dcc30f3ad9195379fc03aba0142466b73323
Ornlu2 <alterdylan@gmail.com>
2017-08-13 16:36:34 -0400

    First Commit

---
+++ Bandido (Train Robbing Game)/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 3.0F;
+    public float rotateSpeed = 3.0F;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+    }
+}
---
+++ Bandido (Train Robbing Game)/Assets/Scripts/Mover.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public Rail rail;
+    public PlayMode mode;
+
+    public float speed = 2.5f;
+    public bool isReversed;
+    public bool isLooping;
+    public bool PingPong;
+
+	private int currentSeg;
+	private float transition;
+	private bool isCompleted;
+
+    public int PositionofCar;
+
+   
+
+    private void Update()
+	{
+		if(!rail)
+		{
+			return;
+		}
+		if(!isCompleted)
+		{
+			Play (!isReversed);
+		}
+	}
+	private void Play( bool forward = true)
+	{
+        float m = (rail.nodes[currentSeg + 1].position - rail.nodes[currentSeg].position).magnitude;
+        float s = (Time.deltaTime * 1 / m) * speed;
+
+        transition += (forward) ? s : -s;
+
+		if (transition>1)
+		{
+			transition = 0;
+			currentSeg++;
+            if(currentSeg == rail.nodes.Length-1)
+            {
+                if(isLooping)
+                {
+                    if (PingPong)
+                    {
+                        transition = 1;
+                        currentSeg = rail.nodes.Length - 2;
+                        isReversed = !isReversed;
+                    }
+                    else
+                    {
+                        currentSeg = 0;
+                    }
+                }
+                else
+                {
+                    isCompleted = true;
+                    return;
+                }
+            }
+		}
+		else if (transition<0)
+		{
+            transition = 1;
+            currentSeg--;
+            if (currentSeg == rail.nodes.Length - 1)
+            {
+                if (isLooping)
+                {
+                    if (PingPong)
+                    {
+                        transition = 0;
+                        currentSeg = 0;
+                        isReversed = !isReversed;
+                    }
+                    else
+                    {
+                        currentSeg = currentSeg = rail.nodes.Length- 2; 
+                    }
+                }
+                else
+                {
+                    isCompleted = true;
+                    return;
+                }
+            }
+        }
+
+      
+         
+
+      
+            transform.position = rail.PositionOnRail(currentSeg - PositionofCar , transition, mode);
+            transform.rotation = rail.Orientation(currentSeg - PositionofCar, transition);
+        
+    }
+
+}
